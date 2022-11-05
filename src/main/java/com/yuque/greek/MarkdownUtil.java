cbd3d1b8a1dc6d8f3d4d920dc4f171e3f3cb7270
greek <zhaofengzhou9@gmail.com>
2022-11-05 21:53:54 +0800

    添加markdown 解析工具

---
+++ src/main/java/com/yuque/greek/MarkdownUtil.java
@@ -0,0 +1,69 @@
+package com.yuque.greek;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MarkdownUtil {
+
+    private static final Pattern IMAGE_REGEX = Pattern.compile("!\\[(.*)\\]\\((.*)\\)");
+
+    public static List<Image> getAllImage(String markdown) {
+        Matcher matcher = IMAGE_REGEX.matcher(markdown);
+        List<Image> result = new ArrayList<>();
+        while (matcher.find()) {
+            result.add(new Image(matcher.group(1), matcher.group(2)));
+        }
+        return result;
+    }
+
+    public static void downloadImage(String name, String url) {
+        System.out.println(url);
+        try {
+            BufferedImage read = ImageIO.read(new URL(url));
+            ImageIO.write(read, "png", Path.of(name).toFile());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    static class Image {
+
+        private String name;
+        private String url;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public Image(String name, String url) {
+            this.name = name;
+            this.url = url;
+        }
+    }
+
+
+}
--- src/main/java/com/yuque/greek/Yuque.java
+++ src/main/java/com/yuque/greek/Yuque.java
@@ -1,10 +1,13 @@
 package com.yuque.greek;
 
+import com.yuque.greek.entity.resp.Doc;
 import com.yuque.greek.entity.resp.Repo;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -14,14 +17,20 @@ public class Yuque {
 
 
     public static void main(String[] args) throws IOException {
-        Properties properties = new Properties();
-        properties.load(new FileReader("token.properties"));
+//        Properties properties = new Properties();
+//        properties.load(new FileReader("token.properties"));
+//
+//        String accessToken = properties.getProperty("token");
+//        YuqueClient yuqueClient = new YuqueClient("greek-zzf", accessToken);
+//
+//        Doc docDetail = yuqueClient.getDocDetail(1927824, 97759346);
+//        System.out.println(docDetail);
+//
+//        Files.writeString(Path.of(docDetail.getTitle() + ".md"), docDetail.getMarkdownContent());
 
-        String accessToken = properties.getProperty("token");
-        YuqueClient yuqueClient = new YuqueClient("greek-zzf", accessToken);
 
-        System.out.println(yuqueClient.getDocList(1927824));
-
-        System.out.println(yuqueClient.getDocDetail(1927824, 97759346));
+        String markdown = Files.readString(Path.of("Git Bisect 快速上手.md"));
+        List<MarkdownUtil.Image> allImage = MarkdownUtil.getAllImage(markdown);
+        allImage.stream().forEach(e -> MarkdownUtil.downloadImage(e.getName(), e.getUrl()));
     }
 }
